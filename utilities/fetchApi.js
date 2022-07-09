729de8bd41a4a2ac35cf76f0d5af3e579a0028ea
unknown <mukhtaaramiin@gmail.com>
2022-07-09 11:41:50 +0300

    env file

--- utilities/fetchApi.js
+++ utilities/fetchApi.js
@@ -5,7 +5,7 @@ export const baseURL = "https://bayut.p.rapidapi.com";
 export const fetchApi = async (url) => {
   const { data } = await axios.get(url, {
     headers: {
-      "X-RapidAPI-Key": "a0b20f3c4bmshcba5658ec7dd6dap160973jsnfaf22ca6b038",
+      "X-RapidAPI-Key": process.env.RAPID_KEY,
       "X-RapidAPI-Host": "bayut.p.rapidapi.com",
     },
   });
